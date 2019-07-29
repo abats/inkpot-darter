4fd3a532470f62065990b761b3e79ce71f362a6e
Arno Bats <arnobats@gmail.com>
2019-07-29 15:23:07 +0200

    fixed styles

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
   <div id="app">
-<!--    <div id="nav">-->
-<!--      <router-link to="/">Home</router-link> |-->
-<!--      <router-link to="/about">About</router-link>-->
-<!--    </div>-->
-    <router-view/>
+    <div class="dart-wrap">
+      <header class="dart-header">Inkpot darter</header>
+      <section class="dart-content">
+        <div class="dart-columns">
+          <div class="dart-main">
+            <router-view/>
+          </div>
+        </div>
+      </section>
+    </div>
   </div>
 </template>
 
@@ -12,6 +17,7 @@
 
 body{
   background-color: navajowhite;
+   margin: 0;
 }
 
 #app {
@@ -34,4 +40,33 @@ body{
 #nav a.router-link-exact-active {
   color: #42b983;
 }
+
+.dart-wrap{
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+
+.dart-header, .dart-footer{
+  height: 50px;
+  font-size: 24px;
+  text-transform: uppercase;
+  color: #484646;
+}
+
+.dart-content {
+  flex: 1;
+  display: flex;
+  color: #000;
+}
+
+.dart-columns{
+  display: flex;
+  flex: 1;
+}
+
+.dart-main{
+  flex: 1;
+}
+
 </style>
